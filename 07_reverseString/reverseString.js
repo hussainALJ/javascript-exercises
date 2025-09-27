6b097195bff1ce30c109762029c8dfb45e6f0134
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2025-09-27 05:34:33 +0300

    Finish 07_reverseString

--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    let reversStr = "";
+    for (let i = str.length - 1; i >= 0; i--) {
+        reversStr += str.charAt(i);
+    }
+    return reversStr;
 };
 
 // Do not edit below this line
