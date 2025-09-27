0b08b01e7f813a223ef5bd155318784d7ecb9540
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2025-09-27 07:32:27 +0300

    Finish 09_sumAll

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(a, z) {
+    let sumAtoZ = 0;
+    if ((a < 0 || z < 0) || (!Number.isInteger(a) || !Number.isInteger(z))) {
+        sumAtoZ = "ERROR";
+    }else if (a < z) {
+        for (let i = a; i <= z; i++) {
+          sumAtoZ += i;
+        }
+    }else if (a > z){
+        for (let i = z; i <= a; i++){
+            sumAtoZ += i;
+        }
+    }
+    return sumAtoZ;
 };
 
 // Do not edit below this line
