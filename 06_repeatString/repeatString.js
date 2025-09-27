47989fa9fdb3ed5ed2aec10279290bcbd4b7453b
hussainALJ <149183695+hussainALJ@users.noreply.github.com>
2025-09-27 05:19:55 +0300

    Finish 06_repeatString

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,6 +1,16 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatStr = "";
+    let i = 0;
+    do {
+        if (num > 0){
+        repeatStr += str;
+        }else if(num < 0){
+            repeatStr = "ERROR";
+            break;
+        }
+        i++;
+    }while (i < num) 
+    return repeatStr;
 };
-
 // Do not edit below this line
 module.exports = repeatString;
