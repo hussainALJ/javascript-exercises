3252a68d0476ebba79739571f3bbea22bd7f348a
hussainAlJ <149183695+hussainALJ@users.noreply.github.com>
2025-10-02 16:12:36 +0300

    Finish 16_findTheOldest

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    let sortedArr = arr
+    .map(obj => {
+        if (obj.yearOfDeath === undefined) {
+            obj.yearOfDeath = new Date().getFullYear();
+            return obj;
+        }else {
+            return obj;
+        }
+    })
+    .sort((a, b) => - (a.yearOfDeath - a.yearOfBirth) + (b.yearOfDeath - b.yearOfBirth));
+    let oldest = sortedArr[0];
+    console.log(oldest);
+    return oldest;
 };
 
 // Do not edit below this line
