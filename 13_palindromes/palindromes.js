3199436e8c9fb0877438a844f4828977f8102a29
hussainAlJ <149183695+hussainALJ@users.noreply.github.com>
2025-10-02 13:07:42 +0300

    Finish 13_palindromes

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (rawStr) {
+    let okChars = "abcdefghijklmnopqrstuvwxyz1234567890";
 
+    let str = rawStr
+    .toLowerCase()
+    .split('')
+    .reduce((newStr, char) => (okChars.includes(char)) ? newStr += char: newStr)
+    
+    let reverseStr = str.split('').reverse().join('');
+
+    return str === reverseStr;
 };
 
 // Do not edit below this line
