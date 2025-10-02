cba78f2293bff4ce4b22aa3d272e482b6134a0fa
hussainAlJ <149183695+hussainALJ@users.noreply.github.com>
2025-10-02 15:36:25 +0300

    Finish 14_fibonacci

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(member) {
+    if (member < 0) {
+        return "OOPS";
+    }
+    let num = Number(member);
+    let fibonacciArr = [0, 1];
+    for (let i = 0; fibonacciArr.length <  num + 1; i++) {
+        fibonacciArr.push(fibonacciArr[i] + fibonacciArr[i + 1])
+    }
+    return fibonacciArr[num];
 };
 
 // Do not edit below this line
